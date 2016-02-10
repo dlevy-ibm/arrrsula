517c832aee0134e83a5c958884a41073cf16b0c8
Monty Taylor <mordred@inaugust.com>
2016-02-09 19:55:51 -0600

    Change scripts to use force update

--- clone-source.sh
+++ clone-source.sh
@@ -12,11 +12,9 @@ fi
 cd $BASEDIR
 
 # Update from upstream
+git checkout master
 git fetch https://git.openstack.org/openstack/openstack.git
 git reset --hard FETCH_HEAD
-
-# Null merge previous deploy branch changes if they exist
-git branch -a | grep origin/pyrrrat/deploy && git merge -s ours -m 'Merging up with upstream' origin/pyrrrat/deploy
 
 for project in $PURE_PROJECTS ; do
     git submodule update --init $project
@@ -24,6 +22,10 @@ done
 
 
 for project in $LOCAL_PROJECTS ; do
+    pushd $project
+    git checkout master
+    popd
+
     # Get the pure upstream version checked out
     git submodule update --init $project
 
@@ -33,12 +35,11 @@ for project in $LOCAL_PROJECTS ; do
     git remote | grep pyrrrat || git remote add pyrrrat git@github.com:pyrrrat/${project}.git
     git remote update
 
-    # If we already have a deploy branch, null merge it so we can just push
-    git branch -a | grep pyrrrat/pyrrrat/deploy && git merge -s ours -m 'Merging up with upstream' pyrrrat/pyrrrat/deploy
-
     # Merge our change
     git fetch git@github.com:pyrrrat/${project}.git
     git merge --no-edit remotes/pyrrrat/master
+
+    git checkout -B deploymaybe
     popd
 
     # Update the submodule to reference our repo
@@ -65,5 +66,8 @@ for project in $EXTRA_PROJECTS ; do
     git reset --hard origin/master
     popd
 done
+
+git checkout -B deploymaybe
+git commit -a -m"Committing potential deploy state"
 
 echo You have sourcecode - you should test it!
--- publish-deploy.sh
+++ publish-deploy.sh
@@ -7,16 +7,15 @@ source project-list.sh
 BASEDIR=$(pwd)/openstack
 
 cd $BASEDIR
+
+# Update pyrrrat/openstack with pure upstream
 git push origin master
 
 # Record the successful state into the repo that is good to pull from
 for project in $LOCAL_PROJECTS ; do
     pushd $project
-    git checkout -b deploymaybe
-    git push pyrrrat HEAD:pyrrrat/deploy
+    git push -f pyrrrat HEAD:pyrrrat/deploy
     popd
 done
 
-git commit -a -m"Committing potential deploy state"
-git checkout -b deploymaybe
-git push origin HEAD:pyrrrat/deploy
+git push -f origin HEAD:pyrrrat/deploy
