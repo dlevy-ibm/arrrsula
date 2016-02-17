d27e751b0144e85e6aea3ead27f88fccab8efb8a
Kyle Mestery <mestery@mestery.com>
2016-02-17 09:09:25 -0600

    builddeps: Add additional dependencies to generate OVS deb files
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- install-builddeps.sh
+++ install-builddeps.sh
@@ -6,4 +6,6 @@ source project-list.sh
 BASEDIR=$(pwd)/openstack
 
 sudo apt-get update
-sudo apt-get install -y build-essential fakeroot graphviz python-all python-qt4 python-twisted-conch
+sudo apt-get install -y build-essential fakeroot graphviz python-all \
+    python-qt4 python-twisted-conch dch devscripts pkg-components \
+    quilt dh-make-perl dh-autoreconf libssl-dev
