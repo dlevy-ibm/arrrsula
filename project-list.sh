1469d3aa2d7294b48e4640752b73aad03d561967
Kyle Mestery <mestery@mestery.com>
2016-02-17 08:51:21 -0600

    project-list.sh: Make all project variables conditional
    This allows them to be overriden, specifically for our jenkins jobs.
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- project-list.sh
+++ project-list.sh
@@ -1,3 +1,4 @@
-PURE_PROJECTS='cinder glance heat horizon keystone neutron requirements'
-LOCAL_PROJECTS='nova python-ironicclient ironic ironic-python-agent ovs'
-EXTRA_PROJECTS='kuryr networking-ovn'
+# NOTE(mestery): Allow these to be overriden so we can set these in our jenkins jobs
+PURE_PROJECTS=${PURE_PROJECTS:-'cinder glance heat horizon keystone neutron requirements'}
+LOCAL_PROJECTS=${LOCAL_PROJECTS:-'nova python-ironicclient ironic ironic-python-agent ovs'}
+EXTRA_PROJECTS=${EXTRA_PROJECTS:-'kuryr networking-ovn'}
